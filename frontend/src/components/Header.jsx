9a82641ce7d23b95577c337df820b57b27f469c5
skvelmurugan <skvelmurugan19@gmail.com>
2024-09-17 22:38:41 +0530

    mob nav

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import { MdOutlineShoppingCart, MdMenu } from "react-icons/md";
+import { MdOutlineShoppingCart } from "react-icons/md";
 import logo from "../assets/amazonlogo.svg"
+import Mobilenav from "./Mobilenav"
 
 function Header() {
   return (
-    <div className='flex w-full h-16 px-3 md:px-0 md:justify-around items-center py-3 shadow-lg flex-1'>
+    <div className='flex w-full h-16 px-4 md:px-14 justify-between items-center py-3 shadow-lg flex-1'>
       <div className='logo-div flex items-center'>
-        <MdMenu className='md:hidden mr-2 text-2xl cursor-pointer text-[#2257bf]' />
+        <Mobilenav className="md:hidden" />
         <img src={logo} className='h-12 w-23' alt="logo" />
       </div>
       <div className='hidden md:block'>
         <input type="search" className='rounded-full w-80 border outline-none hover:border-[#2257bf] py-2 px-3 ml-2 h-9' placeholder='Search here...' />
       </div>
-      <div className="user-div hidden md:block">
+      <div className="user-div">
         <div className="flex items-center">
           <MdOutlineShoppingCart className='text-2xl d-block cursor-pointer text-[#2257bf]' />
-          <button className='ml-2 bg-[#2257bf] py-2 px-3 text-white rounded-lg'>Login / Sign up</button>
+          <button className='ml-2 bg-[#2257bf] py-2 px-3 text-white rounded-lg hidden md:block'>Login / Sign up</button>
         </div>
       </div>
     </div>
--- frontend/src/components/Mobilenav.jsx
+++ frontend/src/components/Mobilenav.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MdMenu, MdClose } from "react-icons/md";
 
 function Mobilenav() {
+  const [expand, setExpand] = useState(false);
+  const handleSidebar = () => {
+    setExpand(true); 
+  }
+
   return (
-    <div>
-      
+    <>
+    <MdMenu className='md:hidden mr-2 text-2xl cursor-pointer text-[#2257bf]' onClick={handleSidebar} />
+    <div id="sideNav" className={`fixed h-screen top-0 px-4 py-6 ${expand ? 'w-3/4 left-0' : 'w-0 left-[-35px]'} ease-in-out z-2 duration-300 overflow-hidden bg-gray-600`}> 
+      <div className="flex justify-end">
+        <MdClose className='text-2xl text-white cursor-pointer' onClick={()=> setExpand(false)}/>
+      </div>
+      <div className='mt-2 flex'>
+        <button className='w-full bg-[#2257bf] py-2 px-3 text-white rounded-lg'>Login / Sign up</button>
+      </div>
     </div>
+    </>
   )
 }
 
