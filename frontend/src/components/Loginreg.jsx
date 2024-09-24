2f87d5a54d23bf40d1369e8a7e6ddf773d1b82d1
skvelmurugan <skvelmurugan19@gmail.com>
2024-09-24 22:18:09 +0530

    validations login & signup

--- frontend/src/components/Loginreg.jsx
+++ frontend/src/components/Loginreg.jsx
@@ -2,20 +2,50 @@ import React, {useState} from 'react'
 
 function Loginreg() {
   const [islogin, setIslogin] = useState(true);
-  const [userdata, setUserdata] = useState({name:'', email:'', password:'', confirmpassword:''});
+  const [userdata, setUserdata] = useState({username:'', email:'', password:'', confirmpassword:''});
   const [logindata, setLogindata] = useState({username:'', password:''});
   const [err, setErr] = useState({});
-  const [error, setError] = useState({})
+  const [error, setError] = useState({});
+  const pwdregx = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/
 
   const setValues = (e) => {
     e.preventDefault()
-    const { name, value } = e.target;
+    const { name, value } = e.target; 
     setUserdata((prevFormData) => ({ ...prevFormData, [name]: value })); 
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const errors = validateSignupdata(userdata);
+    setErr(errors)
     console.log('hai', userdata)
+  }
+
+  const validateSignupdata = (data) => {
+    const error={};
+    if(!data.username.trim()){
+      error.username = 'Username is required!'
+    }
+
+    if(!data.email.trim()){
+      error.email = 'Email is required!'
+    }
+
+    if(!data.password.trim()){
+      error.password = 'Password is required!'
+    }  else if(!pwdregx.test(data.password.trim())) {
+      error.password = 'Password is can include one alphanumeric, one lowercase, one uppercase, one special character. password must minimum 9 characters!'
+    }
+
+    if(!data.confirmpassword.trim()){
+      error.confirmpassword = 'Confirm password is required!'
+    } else if(!pwdregx.test(data.confirmpassword.trim())) {
+      error.confirmpassword = 'Confirm Password is can include one alphanumeric, one lowercase, one uppercase, one special character. password must minimum 9 characters!'
+    } else if(data.password.trim() != data.confirmpassword.trim()) {
+      error.confirmpassword = 'Password and Confirm password does not match!'
+    }
+
+    return error;
   }
 
   const changeValues = (e) => {
@@ -39,7 +69,11 @@ function Loginreg() {
 
     if(!data.password.trim()){
       error.password = 'Password is required!'
+    } else if(!pwdregx.test(data.password.trim())) {
+      error.password = 'Password is can include one alphanumeric, one lowercase, one uppercase, one special character. password must minimum 9 characters!'
     }
+
+    return error;
   }
 
   return (
@@ -49,37 +83,37 @@ function Loginreg() {
         {!islogin ? (<div className='px-2 py-3'>
           <form onSubmit={handleSubmit}>
           <div className='my-2'>
-            <input type="text" placeholder='Username' name='name' value={userdata.name} onChange={setValues} className='w-full outline-0 border-b-2 px-3 py-2'/>
-            <span className='text-red name-err'></span>
+            <input type="text" placeholder='Username' name='username' value={userdata.username} onChange={setValues} className='w-full outline-0 border-b-2 px-3 py-2'/>
+            {err.username && <span className='text-red-400 text-sm'>{err.username}</span>}
           </div>
           <div className="my-2">
             <input type="email" placeholder='E-mail' name='email' value={userdata.email} onChange={setValues} className='w-full outline-0 border-b-2 px-3 py-2'/>  
-            <span className='text-red email-err'></span>
+            {err.email && <span className='text-red-400 text-sm'>{err.email}</span>}
           </div>
           <div className="my-2">
             <input type="password" placeholder='Password' name='password' value={userdata.password} onChange={setValues} className='w-full outline-0 border-b-2 px-3 py-2'/>
-            <span className='text-red password-err'></span>
+            {err.password && <span className='text-red-400 text-sm'>{err.password}</span>}
           </div>
           <div className='my-2'>
             <input type="password" placeholder='Confirm Password' name='confirmpassword' value={userdata.confirmpassword} onChange={setValues} className='w-full outline-0 border-b-2 px-3 py-2'/>
-            <span className='text-red confirmpassword-err'></span>
+            {err.confirmpassword && <span className='text-red-400 text-sm'>{err.confirmpassword}</span>}
           </div>
           <button type='submit' className='w-full rounded-full bg-[#2257bf] text-white py-2 mt-4'>Sign up</button>
           </form>
-          <p className='my-4'>Already have an account? <span className='text-base font-bold text-[#0066c0] cursor-pointer' onClick={()=>setIslogin(true)}>Login</span></p>
+          <p className='my-4'>Already have an account? <span className='text-base font-bold text-[#0066c0] cursor-pointer' onClick={()=>setIslogin(!islogin)}>Login</span></p>
         </div>) : (<div className='px-2 py-3'>
           <form onSubmit={handleLogin}>
             <div className='my-3'>
               <input type="text" className='w-full outline-0 border-b-2 px-3 py-2' name="username" onChange={changeValues} placeholder='Username' />
-              {error.username && <span className='text-red'>{error.username}</span>}
+              {error.username && <span className='text-red-400 text-sm'>{error.username}</span>}
             </div>
             <div className='my-3'>
               <input type="password" className='w-full outline-0 border-b-2 px-3 py-2' name="password" onChange={changeValues} placeholder='Password' />
-              {error.password && <span className='text-red'>{error.password}</span>}
+              {error.password && <span className='text-red-400 text-sm'>{error.password}</span>}
             </div>
             <button type='submit' className='w-full rounded-full bg-[#2257bf] text-white py-2 mt-4'>Login</button>
           </form>
-          <p className='my-4'>Create your amazon account? <span className='text-base font-bold text-[#0066c0] cursor-pointer' onClick={()=>setIslogin(false)}>Sign up</span></p>
+          <p className='my-4'>Create your amazon account? <span className='text-base font-bold text-[#0066c0] cursor-pointer' onClick={()=>setIslogin(!islogin)}>Sign up</span></p>
         </div>)}                       
       </div>
     </div>
